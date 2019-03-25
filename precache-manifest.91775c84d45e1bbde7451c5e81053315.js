1cd7608670207811f76374dd68010c63c38389a5
faviansya <faviansya@alphatech.id>
2019-03-26 05:48:38 +0700

    React Build

---
+++ precache-manifest.91775c84d45e1bbde7451c5e81053315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "35f08246b56d853f107e",
+    "url": "/static/js/main.5dfadd83.chunk.js"
+  },
+  {
+    "revision": "7067a7b6fedeabdcf4cf",
+    "url": "/static/js/2.eb97df7a.chunk.js"
+  },
+  {
+    "revision": "35f08246b56d853f107e",
+    "url": "/static/css/main.c6af1ca1.chunk.css"
+  },
+  {
+    "revision": "58b5b965016f0644d035526b0f5fec12",
+    "url": "/index.html"
+  }
+];
